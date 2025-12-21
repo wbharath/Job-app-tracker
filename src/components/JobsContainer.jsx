1c92351bcecd3e98ae8e5c5dd534aebf51ab0589
wbharath <wbharath51@gmail.com>
2025-12-21 15:27:57 -0400

    get all jobs component created

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-
+import { useDispatch, useSelector } from 'react-redux'
+import Wrapper from '../assets/wrappers/JobsContainer'
+import Job from './Job'
+import Loading from './Loading'
 const JobsContainer = () => {
+  const { jobs, isLoading } = useSelector((store) => store.allJobs)
+  const dispatch = useDispatch()
+  if (isLoading) {
+    return <Loading center />
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>..No jobs to display</h2>
+      </Wrapper>
+    )
+  }
   return (
-    <div>
-      Jobs Container
-    </div>
+    <Wrapper>
+      <h5>Jobs Info</h5>
+      <div className="jobs">
+        {jobs.map((job) => {
+          console.log(job)
+          return <Job key={job._id} {...job} />
+        })}
+      </div>
+    </Wrapper>
   )
 }
 
